675e4d0176c663b81a6787c1144e329f61b1ccec
ALex Loper <acloper11@gmail.com>
2021-09-15 17:29:04 -0500

    ConsoleResume

---
+++ app.js
@@ -0,0 +1,23 @@
+let resume = {
+  Name: "Alex Loper".toUpperCase(),
+  Career: "Warehouse Manager",
+  description: "I run one fo the Armstrong Relocation warehouses in downtown Birmingham.",
+  intrests: ["I like to play video games", "I enjoy playing soccer", "I like watching football on the weekends"],
+  pastPositions: [
+    companyName,
+    "Felinis Pizza",
+    jobTitle,
+    "chef",
+    description,
+    "I would make pizza for custmers",
+    // "deliveries department at Home Depot, I would get everything ready to be deleivred to customers",
+    // "debt collector at Zealandia capital, I would cold call people who owed money agaisnt their timeshares that they owned",
+  ],
+  skills: ["BAM: Grand Champion Rocket League palyer", "I can touch my tounge to my nose", "spanish", "BAM: built my own PC", "film buff", "BAM: scratch golfer"],
+};
+
+function displayPosition(pastPositions) {
+  return companyName, jobTitle, description;
+}
+
+console.log(resume);
